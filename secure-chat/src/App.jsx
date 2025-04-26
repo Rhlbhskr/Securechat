1df93e7e98a72d3f1aab8f692fd826662e064af0
Rahul Bhaskar <148891548+Rhlbhskr@users.noreply.github.com>
2025-04-26 23:13:34 +0400

    Update App.jsx

--- secure-chat/src/App.jsx
+++ secure-chat/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import forge from 'node-forge';
 
--const socket = io('http://localhost:5000');
 + const socket = io('http://securechat-production.up.railway.app');
 
 const generateKeyPair = () =>
