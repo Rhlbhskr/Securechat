cf2e0809d13dc17e87b85247a1c07b7a15f377da
Rahul Bhaskar <148891548+Rhlbhskr@users.noreply.github.com>
2025-04-27 10:14:34 +0400

    Update index.js

--- secure-chat/server/index.js
+++ secure-chat/server/index.js
@@ -11,10 +11,6 @@ const io = new Server(server, {
   },
 });
 
-io.on('connection', (socket) => {
-  console.log(`New user connected: ${socket.id}`);
-});
-
 app.use(cors());
 app.use(express.json());
 
@@ -75,7 +71,7 @@ io.on('connection', (socket) => {
     })));
   });
 });
+
 server.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
-
